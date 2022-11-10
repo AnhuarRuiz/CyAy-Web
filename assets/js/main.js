589e29e1157d3e2b051339a4cfc73a989066f0d9
Anhuar Ruiz <pain.dev@outlook.com>
2022-11-10 02:32:21 -0600

    Update - 6

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,31 +27,15 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 
-  /**
-   * Navbar links active state on scroll
+   /**
+   * Mobile nav toggle
    */
-  let navbarlinks = document.querySelectorAll('#navbar .scrollto');
-
-  function navbarlinksActive() {
-    navbarlinks.forEach(navbarlink => {
-
-      if (!navbarlink.hash) return;
-
-      let section = document.querySelector(navbarlink.hash);
-      if (!section) return;
-
-      let position = window.scrollY;
-      if (navbarlink.hash != '#header') position += 200;
-
-      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-        navbarlink.classList.add('active');
-      } else {
-        navbarlink.classList.remove('active');
-      }
+    on('click', '.mobile-nav-toggle', function(e) {
+      select('#navbar').classList.toggle('navbar-mobile')
+      this.classList.toggle('bi-list')
+      this.classList.toggle('bi-x')
     })
-  }
-  window.addEventListener('load', navbarlinksActive);
-  document.addEventListener('scroll', navbarlinksActive);
+  
 
   /**
    * Function to scroll to an element with top ofset
